c7c5791b5c1a74193c08a09be81285654d40ca29
GJusto <gustavo.miranda01@fatec.sp.gov.br>
2020-06-25 00:42:55 -0300

    entidades

---
+++ src/entity/Musica.java
@@ -0,0 +1,35 @@
+package entity;
+
+public class Musica {
+	
+	private String nome;
+	private String album;
+	private int notaAvaliacao;
+	private String letra;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getAlbum() {
+		return album;
+	}
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+	public int getNotaAvaliacao() {
+		return notaAvaliacao;
+	}
+	public void setNotaAvaliacao(int notaAvaliacao) {
+		this.notaAvaliacao = notaAvaliacao;
+	}
+	public String getLetra() {
+		return letra;
+	}
+	public void setLetra(String letra) {
+		this.letra = letra;
+	}
+
+}
